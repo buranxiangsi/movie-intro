971bf2f846cee5c2108f73a47708ef0dfceaa7c9
ling <l13789060033@163.com>
2022-02-08 18:43:16 +0800

    notice undefined 优化

--- test.js
+++ test.js
@@ -14,18 +14,19 @@ https.get(url, function (res) {
     let allFiles = [];
     let html = iconv.decode(Buffer.concat(chunks), 'utf-8');
     let $ = cheerio.load(html);
-    $('#showing-soon .item').each(function() {
-      const title = $('.intro h3 a',this).text();
-      const data = $('.intro ul li',this).eq(0).text()
-      const type =  $('.intro ul li',this).eq(1).text()
-      const notice = $('ul a',this).attr('href')
+    $('#showing-soon .item').each(function () {
+      const title = $('.intro h3 a', this).text();
+      const data = $('.intro ul li', this).eq(0).text();
+      const type = $('.intro ul li', this).eq(1).text();
+      const notice = $('ul a', this).attr('href') || null;
       allFiles.push({
         title,
         data,
         type,
-        notice
+        notice,
       });
     });
+    console.log(allFiles);
     fs.writeFile('./fils.json', JSON.stringify(allFiles), function (err) {
       if (!err) {
         console.log('文件写入完毕');
